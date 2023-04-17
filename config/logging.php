ce3e63ee0cf74f73af3f9a92c611d4a60d9b360e
Md. Yamin Hossain <needyamin@gmail.com>
2023-04-17 12:10:54 +0600

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -56,13 +56,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/YAMiN.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/YAMiN.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
@@ -112,7 +112,7 @@ return [
         ],
 
         'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/YAMiN.log'),
         ],
     ],
 
